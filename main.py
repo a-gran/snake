56c7f702d287d40a7b539e8cc507b579f638f958
Aristarh Gran <awebdeff@gmail.com>
2023-09-15 22:49:54 +0300

    add CONST's

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ from random import randrange
 SCREEN_SIDE = 1000 # width and height of the screen
 SIZE_ONE_CUBE = 50 # snake size
 
+x, y = randrange(0, SCREEN_SIDE, SIZE_ONE_CUBE), randrange(0, SCREEN_SIDE, SIZE_ONE_CUBE) # snake's initial position
+apple = randrange(0, SCREEN_SIDE, SIZE_ONE_CUBE), randrange(0, SCREEN_SIDE, SIZE_ONE_CUBE) # apple's initial position
+dictionary_snake_move = {'W': True, 'S': True, 'A': True, 'D': True} # snake's movements
+length = 1 # snake's length
+score = 0 # game score
+fps = 5 # fps and the speed of the snake, that will grow up with the score
+snake = [(x, y)] # snake's position
+dx, dy = 0, 0 # snake's move direction
