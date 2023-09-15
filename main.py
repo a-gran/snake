808d511ebbae8c087d0b52d055d116c0258b98c4
Aristarh Gran <awebdeff@gmail.com>
2023-09-15 22:31:34 +0300

    add screen

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+# Подключение модулей
 import pygame
+# Константы
+WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = (300, 300)
+# Переменные и инициализация
+pygame.init()
+screen = pygame.display.set_mode(WINDOW_SIZE)
+# Цикл программы
+while True:
+	# Условие закрытия программы
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			exit()
